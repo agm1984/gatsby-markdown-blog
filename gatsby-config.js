c4b49b4190fc2968e989da6a0d141baa809b9419
Adam Mackintosh <agm1984@gmail.com>
2018-09-10 19:06:52 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Markdown Blog',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome</h1>
+    <p>This is a sample site that demonstrates Gatsby JS.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
